76f97b76e31a40f9adafac079f1eea09e93b9531
Sera <sera@netlab.jp>
2017-11-11 19:01:12 +0900

    Add エラー、例外チェック

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -7,10 +7,10 @@ describe 'Version' do
     it "1,4,2を渡したバージョンオブジェクトに対して番号'1.4.2'を返す" do
       expect(version_1_4_2.number).to eq '1.4.2'
     end
-    # it "2,5,3を渡したバージョンオブジェクトに対して番号'2.5.3'を返す" do
-    #   version = Version.new(2,5,3)
-    #   expect(version.number).to eq '2.5.3'
-    # end
+    it "2,5,3を渡したバージョンオブジェクトに対して番号'2.5.3'を返す" do
+      version = Version.new(2,5,3)
+      expect(version.number).to eq '2.5.3'
+    end
   end
 
   describe '等価性チェックをする' do
@@ -22,6 +22,55 @@ describe 'Version' do
     context 'バージョンAはバージョンAと等しい場合はtrueを返す' do
       it 'バージョン1.4.2はバージョン1.4.2と等しい場合はtrueを返す' do
         expect(version_1_4_2 == Version.new(1,4,2)).to be true
+      end
+    end
+  end
+
+  describe 'major, minor, patch がゼロ、または正の整数以外のものが与えた場合例外が発生する' do
+    context '許可しない値の場合' do
+      context 'マイナスの場合エラー' do
+        it 'バージョンオブジェクト生成時に major に -1 を与えると例外が発生する' do
+          expect{ Version.new(-1,1,1) }.to raise_error(ArgumentError, "majorは不正な値です。")
+        end
+        it 'バージョンオブジェクト生成時に minor に -1 を与えると例外が発生する' do
+          expect{ Version.new(1,-1,1) }.to raise_error(ArgumentError, "minorは不正な値です。")
+        end
+        it 'バージョンオブジェクト生成時に patch に -1 を与えると例外が発生する' do
+          expect{ Version.new(1,1,-1) }.to raise_error(ArgumentError, "patchは不正な値です。")
+        end
+      end
+      context '文字列の場合エラー' do
+        it 'バージョンオブジェクト生成時に major に "1" を与えると例外が発生する' do
+          expect{ Version.new("1",1,1) }.to raise_error(ArgumentError, "majorは不正な値です。")
+        end
+        it 'バージョンオブジェクト生成時に minor に "1" を与えると例外が発生する' do
+          expect{ Version.new(1,"1",1) }.to raise_error(ArgumentError, "minorは不正な値です。")
+        end
+        it 'バージョンオブジェクト生成時に patch に "1" を与えると例外が発生する' do
+          expect{ Version.new(1,1,"1") }.to raise_error(ArgumentError, "patchは不正な値です。")
+        end
+      end
+      context 'nilの場合エラー' do
+        it 'バージョンオブジェクト生成時に major に nil を与えると例外が発生する' do
+          expect{ Version.new(nil,1,1) }.to raise_error(ArgumentError, "majorは不正な値です。")
+        end
+        it 'バージョンオブジェクト生成時に minor に nil を与えると例外が発生する' do
+          expect{ Version.new(1,nil,1) }.to raise_error(ArgumentError, "minorは不正な値です。")
+        end
+        it 'バージョンオブジェクト生成時に patch に nil を与えると例外が発生する' do
+          expect{ Version.new(1,1,nil) }.to raise_error(ArgumentError, "patchは不正な値です。")
+        end
+      end
+    end
+    context '許可する値の場合' do
+      it 'バージョンオブジェクト生成時に major に 0 を与えると例外が発生しない' do
+        expect{ Version.new(0,1,1) }.not_to raise_error
+      end
+      it 'バージョンオブジェクト生成時に minor に 0 を与えると例外が発生しない' do
+        expect{ Version.new(1,0,1) }.not_to raise_error
+      end
+      it 'バージョンオブジェクト生成時に patch に 0 を与えると例外が発生しない' do
+        expect{ Version.new(1,1,0) }.not_to raise_error
       end
     end
   end
--- version.rb
+++ version.rb
@@ -1,6 +1,10 @@
 class Version
 
   def initialize(major, minor, patch)
+    check_param(:major, major)
+    check_param(:minor, minor)
+    check_param(:patch, patch)
+
     @major = major
     @minor = minor
     @patch = patch
@@ -10,7 +14,12 @@ class Version
     "#{@major}.#{@minor}.#{@patch}"
   end
 
-  def ==(version)
-    number == version.number
+  def ==(other)
+    number == other.number
   end
+
+  def check_param(key, value)
+    raise ArgumentError.new("#{key}は不正な値です。") unless value.kind_of?(Integer) && value >= 0
+  end
+
 end
